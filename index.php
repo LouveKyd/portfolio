c1b8452b5fbd2a2f344de3ad4998c5cbaa1bbb84
louveKyd <judykaelle.delannoy@gmail.com>
2019-04-12 15:45:20 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="../img/loup.ico" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+    <title> Portfolio </title>
+</head>
+<body>
+<header>
+
+<div id="acceuil" >
+<nav class='narbar fixed-top'>
+</a>
+    <ul>
+        <li> <a href="#acceuil"> Acceuil </a></li>
+        <li><a href="#presentation "> Présentation </a></li>
+        <li><a href="#competence"> Compétence </a></li>
+        <li><a href="#projet"> Projet </a></li>
+        <li><a href="#contact"> Contact </a></li>
+    </ul>
+     <hr>
+</nav>
+<div style="height:5rem;display:block;"> </div>
+</div>
+<div class="loup">
+ <img src="../img/loup.png" alt="loup">
+</div>
+</header>
+<div class="acceuil"> 
+<div class="row">
+        <div class="offset-m-6">
+            <div class="me">
+                <img src="../img/profil.png" alt="mee"> 
+                <h3>Judykaelle Delannoy</h3>
+                <p> Developeur Web junior </p>
+            </div>
+        </div>
+</div>
+
+
+<div id="presentation" class='marge'>
+ </div>
+  <div class='presentation'>
+      <h2> Présentation <img src="../img/pan.png" alt="pan"> </h2> 
+    <hr>
+    <p>
+        Hoi ,je suis Judykaelle Delannoy. J'ai 23 ans.
+      
+    </p>
+</div>
+
+<div id="competence" class='marge2'>
+</div>
+  <div class='competence'>
+    <h2> Compétence </h2>
+    <hr>
+    <h3> HTML <i class="fab fa-html5"></i> </h3>
+    <div class="progress">
+  <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 69%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+<br>
+<h3> CSS <i class="fab fa-css3-alt"></i> </h3>
+<div class="progress">
+  <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: 50%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+<br>
+<h3> PHP <i class="fab fa-php"></i></h3>
+<div class="progress">
+  <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+<br>
+<h3> JavaScript <i class="fab fa-js"></i></h3>
+<div class="progress">
+  <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+<br>
+<h3> Wordpress <i class="fab fa-wordpress"></i> </h3>
+<div class="progress">
+  <div class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+<br>
+<h3> Wordpress <i class="fab fa-wordpress"></i> </h3>
+<div class="progress">
+  <div class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+</div>
+<br>
+
+
+
+<div id="projet" class="marge3">
+</div>
+  <div class="projet">
+    <h2> Projet </h2>
+    <hr>
+    <div class="row">
+    <div class="col"><img src="../img/1.jpg" alt="..." class="rounded-circle">  </div>
+        <div class="col"> <img src="../img/3.jpg" alt="..." class="rounded-circle">  </div>
+    <div class="col"><img src="../img/4.jpg" alt="..." class="rounded-circle"></div>
+    <div class="col"><img src="../img/5.jpg" alt="..." class="rounded-circle"></div>
+  </div>
+  <div class="row">
+    <div class="col"><img src="../img/1.jpg" alt="..." class="rounded-circle">  </div>
+        <div class="col"> <img src="../img/3.jpg" alt="..." class="rounded-circle">  </div>
+    <div class="col"><img src="../img/4.jpg" alt="..." class="rounded-circle"></div>
+    <div class="col"><img src="../img/5.jpg" alt="..." class="rounded-circle"></div>
+  </div>
+
+</div>
+<div id="contact" class="marge4">
+ 
+</div>
+<div class="contact">
+    <h2> Me contacter </h2>
+    <hr>
+  <img src="../img/message" alt="">
+    <i class="fas fa-envelope-open-text"></i> <a target="_blank" href="mailto:"> JD.Simplon.Pro@gmail.com</a>
+  <img src="../img/boite.png" alt="">
+</div>
+
+<footer></footer>
+
+
+
+    <link rel="stylesheet" href="../css/joli.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
+
